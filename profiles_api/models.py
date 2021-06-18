aab1cae58f90f21d94aee94a48ae5f74c7198d84
Noy <noynir100@gmail.com>
2021-06-18 12:30:14 +0300

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -7,13 +7,13 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self, email, name, passwoed=None):
+    def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
@@ -29,7 +29,6 @@ class UserProfileManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
